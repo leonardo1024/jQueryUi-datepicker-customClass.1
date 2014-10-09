ba28323eb3f212a112872e85c0ee65181dfce676
shlomohass <shlomohass@gmail.com>
2014-10-10 00:54:16 +0300

    Updated - removed Errors

--- jQueryUi-datepicker-customClass.1.js
+++ jQueryUi-datepicker-customClass.1.js
@@ -2,10 +2,8 @@
  * jQuery UI Datepicker @VERSION
  *
  * Copyright (c) 2009 AUTHORS.txt (http://jqueryui.com/about)
- * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * http://docs.jquery.com/UI/Datepicker
  *
  * Depends:
  *      jquery.ui.core.js
